3a9960ecf1ac6b53925941ba8d4a8b4f2bf5300a
Mohammad Bilal <mdbilal7620@gmail.com>
2023-01-24 14:18:17 +0530

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
 import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page1";
-import Page3 from "./pages/Page1";
-import Page4 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <BrowserRouter>
+        {/* <BrowserRouter> */}
           <Routes>
-            <Route path="/" element={<Page1/>}>
-              <Route path="page2" element={<Page2 />} />
-              <Route path="page3" element={<Page3 />} />
-              <Route path="page4" element={<Page4 />} />
+              <Route path="/" element={<Page1/>}>
+              <Route path="/page2" element={<Page2 />} />
+              <Route path="/page3" element={<Page3 />} />
+              <Route path="/page4" element={<Page4 />} />
             </Route>
           </Routes>
-        </BrowserRouter>
+        {/* </BrowserRouter> */}
       </header>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,30 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className="header-div">
-      <div>
-        <i className="logo fa fa-google"></i>
+    <div classNameName="header-div">
+      <div className="top-logo-div">
+        
+        <h1> <i className="fa fa-google"></i>Eden</h1>
       </div>
       <div>
-        <Button />
+        <div className="stepper-wrapper">
+          <div className="stepper-item completed">
+            <div className="step-counter">1</div>
+            <div className="step-name">First</div>
+          </div>
+          <div className="stepper-item ">
+            <div className="step-counter">2</div>
+            <div className="step-name">Second</div>
+          </div>
+          <div className="stepper-item active">
+            <div className="step-counter">3</div>
+            <div className="step-name">Third</div>
+          </div>
+          <div className="stepper-item">
+            <div className="step-counter">4</div>
+            <div className="step-name">Forth</div>
+          </div>
+        </div>
       </div>
     </div>
   );
